8882ae11cc0bea7fc27fb209d5240f00ffb7bee2
Zuzanna Urban <zuzanna.s.urban@gmail.com>
2023-12-17 21:29:44 +0100

    add 13.6 requirements

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,20 @@
 import styles from './SearchForm.module.scss'
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
+import { useDispatch } from 'react-redux';
+import { updateSearchString } from '../../redux/store';
 
 const SearchForm = () => {
+    let searchString = '';
+
+    const dispatch = useDispatch();
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch(updateSearchString(searchString));
+    }
     return (
-        <form className={styles.searchForm}>
-            <TextInput placeholder="Search..." />
+        <form form onSubmit={handleSubmit} className={styles.searchForm}>
+            <TextInput placeholder="Search..." onChange={e => searchString=e.target.value}/>
             <Button>
                 <span className="fa fa-search" />
             </Button>
