b47c4ee9e05e31b5b7b7de25b9ded5b1f8f6b871
Zuzanna Urban <zuzanna.s.urban@gmail.com>
2023-12-18 13:52:33 +0100

    add 14.4 requirements

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,18 @@
 import styles from './Card.module.scss';
+import { clsx } from 'clsx';
+import { useDispatch } from 'react-redux';
+import { toggleCardFavorite } from '../../redux/store';
 
 const Card = props => {
+    const dispatch = useDispatch();
+    const handleOnClick = e => {
+       e.preventDefault();
+       dispatch(toggleCardFavorite(props.id));
+    }    
     return (
-        <li className={styles.card}>{props.title}</li>
+        <li className={styles.card}>{props.title}
+            <button class={styles.button} onClick = { handleOnClick } ><i className = {clsx("fa fa-star-o", props.isFavorite && styles.isFavorite)}></i></button>
+        </li>
     );
 };
 
