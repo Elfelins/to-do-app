2808d7a9d5388982017774360ab39fe7ee280c14
Zuzanna Urban <zuzanna.s.urban@gmail.com>
2023-12-18 16:02:04 +0100

    add 14.5 requirements

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,17 +1,24 @@
 import styles from './Card.module.scss';
 import { clsx } from 'clsx';
 import { useDispatch } from 'react-redux';
-import { toggleCardFavorite } from '../../redux/store';
+import { toggleCardFavorite, removeCard } from '../../redux/cardsRedux';
 
 const Card = props => {
     const dispatch = useDispatch();
     const handleOnClick = e => {
        e.preventDefault();
        dispatch(toggleCardFavorite(props.id));
-    }    
+    } 
+    const handleRemove = e => {
+        e.preventDefault();
+        dispatch(removeCard(props.id));
+    }   
     return (
         <li className={styles.card}>{props.title}
-            <button class={styles.button} onClick = { handleOnClick } ><i className = {clsx("fa fa-star-o", props.isFavorite && styles.isFavorite)}></i></button>
+            <div>
+                <button className = {styles.button} onClick = {handleOnClick} ><i className = {clsx("fa fa-star-o", props.isFavorite && styles.isFavorite)}></i></button>
+                <button className = {styles.button} onClick = {handleRemove} ><i className = "fa fa-trash"/></button>
+            </div>
         </li>
     );
 };
